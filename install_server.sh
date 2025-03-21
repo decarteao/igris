22973309536183151a8857ea3ef1de3c5a761696
Decarte <decartecussenha@gmail.com>
2025-03-21 10:44:56 +0100

    make it better

--- install_server.sh
+++ install_server.sh
@@ -4,4 +4,29 @@ sudo su
 cd /root
 apt update -y
 
-# continuar mais comandos depois de compilar
+wget https://github.com/decarteao/igris/raw/refs/heads/main/server/igrisserver
+chmod +x igrisserver
+
+cat > /etc/systemd/system/igrisserver.service <<-END
+[Unit]
+Description=IgrisServer
+After=network.target
+
+[Service]
+ExecStart=/root/igrisserver
+WorkingDirectory=/root/
+StandardOutput=inherit
+StandardError=inherit
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+END
+
+systemctl daemon-reload
+
+systemctl enable igrisserver
+systemctl restart igrisserver
+
+reboot
+
